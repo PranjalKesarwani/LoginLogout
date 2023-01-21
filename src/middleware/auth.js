bbe2417953a9c81890b470a478494e8eb8b29b7c
Pranjal Kesarwani <89082511+PranjalKesarwani@users.noreply.github.com>
2023-01-21 16:52:02 +0530

    wrote comments

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,13 +1,13 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/schema");
 
-const auth = async (req, res, next) => {
+const auth = async (req, res, next) => {  //jaha kahi bi middlewares use kr rahe hai waha next() zrur call kr rahe taki middleware ke baad ka bi kaam ho sake
     try {
         const token = req.cookies.jwt;
         const verifyUser = jwt.verify(token, process.env.SECRET_KEY);
-        console.log(verifyUser);
-
-        const user =await userModel.findOne({ _id: verifyUser._id });
+        console.log("verifyUser is: "+verifyUser); //this returns array of an object which is your document. Authentication yahi complete ho jata hai neeche toh sab logout ke liye ab code likh rahe hai
+ //yaha se logout ka code likhna shuru ho gaya
+        const user =await userModel.findOne({ _id: verifyUser._id }); //is object ki id ki help se whole document nikala
         console.log(user);
 
         req.token = token;
